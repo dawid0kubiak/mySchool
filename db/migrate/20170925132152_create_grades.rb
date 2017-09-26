1e3dd5c78a31a3f596c1450db5dea63ebd56946e
Dawid Kubiak <dawid.kubiak@i-bs.pl>
2017-09-26 13:01:20 +0200

    add grades

---
+++ db/migrate/20170925132152_create_grades.rb
@@ -0,0 +1,11 @@
+class CreateGrades < ActiveRecord::Migration[5.1]
+  def change
+    create_table :grades do |t|
+      t.string :short_name
+      t.string :long_name
+      t.string :value
+
+      t.timestamps
+    end
+  end
+end
