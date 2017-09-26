fbe184289fe87f6d99dc7a18e91adcf6652bd312
Dawid Kubiak <dawid.kubiak@i-bs.pl>
2017-09-26 15:25:33 +0200

    add teachers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925132152) do
+ActiveRecord::Schema.define(version: 20170926130205) do
 
   create_table "grades", force: :cascade do |t|
     t.string "short_name"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 20170925132152) do
     t.string "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "grades_pupils", id: false, force: :cascade do |t|
+    t.integer "grade_id", null: false
+    t.integer "pupil_id", null: false
+  end
+
+  create_table "grades_subjects", id: false, force: :cascade do |t|
+    t.integer "grade_id", null: false
+    t.integer "subject_id", null: false
   end
 
   create_table "pupils", force: :cascade do |t|
@@ -36,6 +46,20 @@ ActiveRecord::Schema.define(version: 20170925132152) do
   create_table "subjects", force: :cascade do |t|
     t.string "name"
     t.string "teacher"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subjects_teachers", id: false, force: :cascade do |t|
+    t.integer "teacher_id", null: false
+    t.integer "subject_id", null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
+    t.string "lastname"
+    t.string "email"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
