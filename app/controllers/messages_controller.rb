39f2ae9faa60ee10afafeecea170a302d84abd93
Dawid Kubiak <dawid.kubiak@i-bs.pl>
2017-11-21 10:03:21 +0100

    Messages views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -31,17 +31,12 @@ class MessagesController < ApplicationController
     message.comments.create(comment_params)
     authorize message
     redirect_to message_path message
-
   end
 
   # GET /messages/new
   def new
     @message = Message.new
     authorize @message
-  end
-
-  # GET /messages/1/edit
-  def edit
   end
 
   # POST /messages
@@ -61,19 +56,6 @@ class MessagesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /messages/1
-  # PATCH/PUT /messages/1.json
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html {redirect_to @message, notice: 'Message was successfully updated.'}
-        format.json {render :show, status: :ok, location: @message}
-      else
-        format.html {render :edit}
-        format.json {render json: @message.errors, status: :unprocessable_entity}
-      end
-    end
-  end
 
   # DELETE /messages/1
   # DELETE /messages/1.json
